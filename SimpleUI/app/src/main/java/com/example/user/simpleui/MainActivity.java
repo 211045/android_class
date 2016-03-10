bceacbce832313f1afd95d8ea461f7a7aa03c806
211045 <u211045@taipower.com.tw>
2016-03-10 20:02:46 +0800

    SharedPreferences
    SharedPreferences

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.user.simpleui;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     EditText editText;
     CheckBox hideCheckBox;
+
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +30,18 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText)findViewById(R.id.editText);
         hideCheckBox = (CheckBox)findViewById(R.id.checkBox);
 
+        sp = getSharedPreferences("setting", Context.MODE_PRIVATE);
+        editor = sp.edit();
+
+        editText.setText(sp.getString("editText", ""));
+
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+                editor.putString("editText", editText.getText().toString());
+                editor.apply();
+
                 if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
                     submit(v);
                     return true;
@@ -46,6 +61,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        hideCheckBox.setChecked(sp.getBoolean("hideCheckBox", false));  //設定hideCheckBox預設為不勾選
+
+        hideCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("hideCheckBox", hideCheckBox.isChecked());  //儲存hideCheckBox的勾選狀態到sp
+                editor.apply();
+            }
+        });
+
     }
 
     public void submit(View view)
@@ -54,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
         String text = editText.getText().toString();
         if (hideCheckBox.isChecked())
         {
-            Toast.makeText(this,text,Toast.LENGTH_LONG).show();
+            Toast.makeText(this, text, Toast.LENGTH_LONG).show();
             textView.setText("********");
             editText.setText("********");
             return;
