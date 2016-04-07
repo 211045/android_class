d03c524d0603a5af2aa79ea7632822d4ba0fac26
211045 <u211045@taipower.com.tw>
2016-04-07 22:02:11 +0800

    Free AWS Parse Server & Mongo DB & Parse Dashboard
    5. AWS Create Account
    6. Deploy Parse To AWS
    7. Configuration : Master Key && App Key
    8. Mongo Lab create account & Mongo URI
    9.  NodeJS install
    10. npm install -g parse-dashboard
    11. Replace OriginTA's parse server by Self-Develop parse server

--- SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/SimpleUIApplication.java
@@ -16,7 +16,14 @@ public class SimpleUIApplication extends Application {
         //另開class設定parse連線
         Parse.enableLocalDatastore(this);
 
-        Parse.initialize(this);
+        //Parse.initialize(this);
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                        .applicationId("76ee57f8e5f8bd628cc9586e93d428d5")
+                        .clientKey(null)
+                        .server("http://parseserver-b3322-env.us-east-1.elasticbeanstalk.com/parse/")
+                        .build()
+        );
 
         FacebookSdk.sdkInitialize(getApplicationContext());
     }
